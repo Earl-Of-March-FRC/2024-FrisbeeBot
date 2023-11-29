12a1c49acfb09dcf95c0728d4cd16ebefd15db24
EomLions <eomlions@gmail.com>
2023-11-29 12:00:20 -0500

    Curve

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -39,9 +39,6 @@ public class Robot extends TimedRobot {
 
   XboxController controller = new XboxController(0);
 
-  SlewRateLimiter leftLimit = new SlewRateLimiter(0.95);
-  SlewRateLimiter rightLimit = new SlewRateLimiter(0.95);
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -58,6 +55,8 @@ public class Robot extends TimedRobot {
     leftBackMotor.setNeutralMode(NeutralMode.Brake);
     rightFrontMotor.setNeutralMode(NeutralMode.Brake);
     rightBackMotor.setNeutralMode(NeutralMode.Brake);
+
+    drive.setDeadband(0.05);
   }
 
 
@@ -113,11 +112,15 @@ public class Robot extends TimedRobot {
     double left = controller.getLeftY();
     double right = controller.getRightY();
 
-    if(Math.abs(left) > 0.3){
-      left = leftLimit.calculate((Math.pow(2, Math.abs(left))-1) * (left >= 0 ? 1 : -1));
+    if(Math.abs(left) > 0.1){
+      left = (Math.pow(2, Math.abs(left))-1) * (left >= 0 ? 1 : -1);
+    } else {
+      left = (Math.pow(2, Math.abs(left)+0.1)-1) * (left >= 0 ? 1 : -1);
     }
-    if(Math.abs(right) > 0.3){
-      right = rightLimit.calculate((Math.pow(2, Math.abs(right))-1) * (right >= 0 ? 1 : -1));
+    if(Math.abs(right) > 0.1){
+      right = (Math.pow(2, Math.abs(right))-1) * (right >= 0 ? 1 : -1);
+    } else {
+      right = (Math.pow(2, Math.abs(right)+0.1)-1) * (right >= 0 ? 1 : -1);
     }
 
     drive.tankDrive(left, right);
